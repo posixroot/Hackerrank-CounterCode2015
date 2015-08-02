5652e6bae14551f8d8d024bc8eb9142a7db0e73b
posixroot <kiranbny@gmail.com>
2015-08-01 20:45:11 -0700

    initial commit

---
+++ src/Solution.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Problem Statement
+
+        A DOTA game has N heroes, each with a distinct rank from [1..N]. In DOTA every formation is characterized as a permutation [1...N] of ranks of players.
+        A formation is Imba when the sum of ranks of every two consecutive players is less than or equal to (N+1). Given N, you are to print the lexicographically smallest permutation of ranks [1...N] that makes the formation Imba.
+
+        Input Format
+
+        The first line will contain an integer T, i.e. the number of the test cases followed by T lines, each containing the value of N.
+
+        Constraints
+        1≤T≤5
+        2≤N≤105
+        Output Format
+
+        T lines each containing the permutation; the numbers in each line should be seperated by a single space.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
